fc35ba8c2049c9221d633a554fd4922cbb83fd4c
dennis-IT <dennis.huy.ta@gmail.com>
2020-11-15 23:42:13 -0500

    Updated app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Axios from 'axios';
+import { v4 as uuidv4 } from 'uuid';
 
-function App() {
+import { Row, Col, Form, Button } from 'react-bootstrap';
+import './App.css';
+import Recipe from './components/Recipe';
+import Alert from './components/Alert';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const App = () => {
+  const [query, setQuery] = useState("");
+  const [recipes, setRecipes] = useState([]);
+  const [alert, setAlert] = useState("");
+
+  const APP_ID = "17f50aee";
+  const APP_KEY = "5f899def8c50016e51eb8f126d95a998";
+  const URL = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  const getData = async () => {
+    if (query !== "") {
+      const result = await Axios.get(URL);
+      if (!result.data.more) {
+        return setAlert("No recipe with such name");
+      }
+      console.log(result);
+      setRecipes(result.data.hits);
+      setAlert("");
+      setQuery("");
+    } else {
+      setAlert("Please fill the box");
+    }
+  };
+
+  const onChange = (event) => {
+    setQuery(event.target.value);
+  };
+
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    getData();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="header text-center">Recipe Searching App</h1>
+      <div>
+        <Row>
+          <Col lg={3}>
+            <Form onSubmit={onSubmit}>
+              <Form.Group controlId="formBasicSearch">
+                <Form.Label>Keyword:</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Example: steak"
+                  autoComplete="off"
+                  onChange={onChange}
+                  value={query}
+                />
+                <Form.Text className="text-muted">
+                  Please enter your lookup recipe in the box
+                </Form.Text>
+              </Form.Group>
+              <Button
+                variant="secondary"
+                type="submit">Search</Button>
+            </Form>
+          </Col>
+          <Col lg={9}>
+            <Row>
+              {recipes !== [] && recipes.map(recipe => {
+                return (
+                  <Col sm={4}>
+                    <Recipe
+                      recipe={recipe}
+                      key={uuidv4()} />
+                  </Col>
+                )
+              })}
+            </Row>
+          </Col>
+        </Row>
+      </div>
     </div>
-  );
-}
+  )
+};
 
 export default App;
